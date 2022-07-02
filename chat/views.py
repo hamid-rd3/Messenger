6a184347fd0dcff16ec631aeadf6dec96177a494
Erfan Rasti <erfanrasty@gmail.com>
2022-07-02 11:52:21 +0430

    Managing html files using "view.py"
    The file "chat/views.py" has been modified to manage html files.

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,11 @@
+"""This file manages the html files for the chat app."""
+
+# Importing libraries
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
 # Create your views here.
+
+# Creating an object of the user model
+
+user = get_user_model()
