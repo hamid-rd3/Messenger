98252726119802ce9215ea8cdaa7d70cdb719d46
Erfan Rasti <erfanrasty@gmail.com>
2022-07-02 23:19:48 +0430

    Implementing 'PersonalChatConsumer' class

--- Messenger/asgi.py
+++ Messenger/asgi.py
@@ -11,20 +11,23 @@ import os
 from django.core.asgi import get_asgi_application
 from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack 
+from channels.auth import AuthMiddlewareStack
 from chat.consumers import PersonalChatConsumer
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Messenger.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Messenger.settings")
 
 application = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    
-    'websocket': AuthMiddlewareStack(
-        URLRouter([
-            # ws: stands for websocket
-            path('ws/<int:id>', PersonalChatConsumer)
-            
-        ]))
-})
+application = ProtocolTypeRouter(
+    {
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                [
+                    # ws: stands for websocket
+                    path("ws/<int:id>", PersonalChatConsumer)
+                ]
+            )
+        )
+    }
+)
--- chat/consumers.py
+++ chat/consumers.py
@@ -10,5 +10,28 @@ class PersonalChatConsumer(AsyncWebsocketConsumer):
     """This class defines the consumer for personal chat."""
 
     async def connect(self):
-        """This method is called when a user is connected to the chat."""
-        pass
+        """Connect the user to the chat."""
+        my_id = self.scope["user"].id
+
+        # kwargs: get the id from websocket url
+        other_user_id = self.scope["url_route"]["kwargs"]["id"]
+
+        # Creating a unique group name for the chat
+        if int(my_id) > int(other_user_id):
+            self.room_name = f"{my_id}-{other_user_id}"
+        else:
+            self.room_name = f"{other_user_id}-{my_id}"
+
+        self.room_group_name = "chat_%s" % self.room_name
+
+        # fmt: off
+        await self.channel_layer.group_add(self.room_group_name,
+                                           self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        """Disconnect the user from the chat."""
+        # self.channel_layer.group_discard(self.room_group_name,
+        await self.channel_layer.group_discard(self.room_group_name,
+                                               self.channel_layer)
